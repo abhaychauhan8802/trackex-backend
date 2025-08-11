7b1d3133c31ba568ae2e631ee848f54a556b73be
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2025-08-11 22:35:57 +0530

    change postgresql connection string

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,11 +2,7 @@ import { Pool } from "pg";
 import "dotenv/config";
 
 export const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASS,
-  host: process.env.PG_HOST,
-  port: Number(process.env.PG_PORT),
-  database: process.env.PG_DATABASE,
+  connectionString: process.env.DATABASE_URL,
 });
 
 pool.on("error", (err, client) => {
